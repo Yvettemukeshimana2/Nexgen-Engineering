1010e688f3346da452a55e5fec4fc5f39fd8757c
Yvettemukeshimana2 <yvettemukeshimana79@gmail.com>
2024-10-24 07:34:38 +0200

    10/24

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
   // App.tsx
- 
+ import "./index.css"
 import { RouterProvider } from 'react-router-dom';
  import routes from './routes/Routes';
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,16 +1,15 @@
  module.exports = {
-   purge: ["./src/**/*.html", "./src/**/*.jsx"],
+   purge: ["./src/**/*.{js,jsx,ts,tsx,html}"], // Adjust to cover all file types
    theme: {
      extend: {
        colors: {
          customGreen: {
-           // Define custom green shades
            50: "#f0fdf4",
            100: "#dcfce7",
            200: "#bbf7d0",
            300: "#86efac",
            400: "#4ade80",
-           500: "#22c55e", // Define the main color
+           500: "#22c55e",
            600: "#16a34a",
            700: "#15803d",
            800: "#166534",
@@ -18,27 +17,25 @@
            950: "#042f2e",
          },
          customTeal: {
-           // Define custom teal shades
            50: "#f0fdfa",
            100: "#ccfbf1",
            200: "#99f6e4",
            300: "#5eead4",
            400: "#2dd4bf",
-           500: "#14b8a6", // Define the main color
+           500: "#14b8a6",
            600: "#0d9488",
            700: "#0f766e",
            800: "#115e59",
            900: "#134e4a",
-           950: "#FE0100",
+           950: "#FE0100", // Check this color, as #FE0100 is not a teal-like color.
          },
          customCyan: {
-           // Define custom cyan shades
            50: "#e3fafc",
            100: "#c5f6fa",
            200: "#99e9f2",
            300: "#66d9e8",
            400: "#3bc9db",
-           500: "#22a6b3", // Define the main color
+           500: "#22a6b3",
            600: "#0d8fa5",
            700: "#0a6e8c",
            800: "#0a4c6e",
